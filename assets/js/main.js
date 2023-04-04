dcdad0f415ada4736b406d8f8c0049c8a8d90521
Lucianor01 <lucianorosario02@gmail.com>
2023-04-04 22:07:46 +0200

    Fix-esercizio-completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,6 @@ Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i 
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
 
-
-
 const numeriCasuali = []
 
 const container = document.querySelector('#container')
@@ -19,17 +17,21 @@ function myRandom() {
     return Math.floor(Math.random() * 100) + 1;
 }
 
+//? CICLO DEI 5 NUMERI RANDOM DA INDOVINARE
 for (let i = 0; i < 5; i++) {
     numeriCasuali.push(myRandom());
     ul.innerHTML += `<li>${numeriCasuali[i]}</li>`;
 }
 
-
+//* CANCELLA I NUMERI DOPO 4,9 SECONDI
 setTimeout(function () {
-    ul.innerHTML = '';
-    
+    ul.innerHTML = "";
+}, 4900);
+
+//! ATTIVA IL PROMPT 5 SECONDI DOPO LA VISUALIZZAZIONE DEI NUMERI
+setTimeout(function () {
     const numeriInseriti = [];
-    
+
     for (let i = 0; i < 5; i++) {
         const numeroInserito = parseInt(prompt('Inserisci il numero che hai visto'));
         numeriInseriti.push(numeroInserito);
@@ -43,7 +45,7 @@ setTimeout(function () {
         }
     }
 
-    const risultato = `Hai indovinato ${numeriIndovinati.length} numeri: ${numeriIndovinati.join(', ')}`;
+    const risultato = `Hai indovinato ${numeriIndovinati.length} numeri/o: ${numeriIndovinati.join(', ')}`;
     alert(risultato);
-    
+
 }, 5000);
